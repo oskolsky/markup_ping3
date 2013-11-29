7da131667a8d900d60f141a6bad3a589edb06c25
Maxim Oskolsky <mo@coalla.ru>
2013-11-29 10:43:17 +0400

    .

---
+++ assets/javascripts/lang.js
@@ -0,0 +1,127 @@
+﻿Lang = Backbone.Model.extend({
+
+  defaults: {
+    RU:  {
+      confidential: "Конфиденциальный",
+      messenger:    "мессенджер",
+      terms:        "Условия использования",
+      feedback:     "Обратная связь",
+      blog:         "Блог"
+    },
+
+    EN:  {
+      confidential: "Confidential",
+      messenger:    "messenger",
+      terms:        "Terms of Use",
+      feedback:     "Feedback",
+      blog:         "Blog"
+    },
+
+    FR:  {
+      confidential: "Messenger",
+      messenger:    "confidentiel",
+      terms:        "Conditions d’utilisation",
+      feedback:     "Commentaires",
+      blog:         "Blog"
+    },
+
+    ES:  {
+      confidential: "Mensajero",
+      messenger:    "confidencial",
+      terms:        "Términos de uso",
+      feedback:     "Comentarios",
+      blog:         "Blog"
+    },
+
+    IT:  {
+      confidential: "Messaggistica",
+      messenger:    "riservata",
+      terms:        "Condizioni per l'utilizzo",
+      feedback:     "Feedback",
+      blog:         "Blog"
+    },
+
+    PT:  {
+      confidential: "Mensageiro",
+      messenger:    "confidencial",
+      terms:        "Termos de utilização",
+      feedback:     "Comentários",
+      blog:         "Blogue"
+    },
+
+    DE:  {
+      confidential: "Vertraulicher",
+      messenger:    "Messenger",
+      terms:        "Nutzungsbedingungen",
+      feedback:     "Feedback",
+      blog:         "Blog"
+    },
+
+    NL:  {
+      confidential: "Vertrouwelijke",
+      messenger:    "messenger",
+      terms:        "Gebruiksvoorwaarden",
+      feedback:     "Feedback",
+      blog:         "Blog"
+    },
+
+    KO:  {
+      confidential: "비공개",
+      messenger:    "메신저",
+      terms:        "서비스 계약",
+      feedback:     "피드백",
+      blog:         "블로그"
+    },
+   
+
+    TH:  {
+      confidential: "เครื่อมือส่งข้อความที่เป็นความลับ",
+      messenger:    "&nbsp;",
+      terms:        "ข้อกำหนดการใช้",
+      feedback:     "คำติชม",
+      blog:         "สมุดบันทึกบนเว็บ"
+    },
+
+    ID:  {
+      confidential: "Pembawa",
+      messenger:    "pesan rahasia",
+      terms:        "Ketentuan penggunaan",
+      feedback:     "Tanggapan",
+      blog:         "Blog"
+    },
+
+    VI:  {
+      confidential: "Tin",
+      messenger:    "bí mật",
+      terms:        "Điều khoản sử dụng",
+      feedback:     "Phản hồi",
+      blog:         "Blog"
+    },
+
+    AR:  {
+      confidential: "مرسل",
+      messenger:    "سري",
+      terms:        "شروط الاستخدام",
+      feedback:     "ملاحظات",
+      blog:         "مدوّنة"
+    },
+
+    ZH:  {
+      confidential: "私密通讯工具",
+      messenger:    "&nbsp;",
+      terms:        "利用規約",
+      feedback:     "反馈",
+      blog:         "博客"
+    },
+
+    JA:  {
+      confidential: "機密メッセンジャー",
+      messenger:    "&nbsp;",
+      terms:        "利用規約",
+      feedback:     "フィードバック",
+      blog:         "ブログ"
+    }
+
+  }
+
+});
