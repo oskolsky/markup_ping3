1b8255813857cdbf8eceba1febedec8564f4f22b
Maxim Oskolsky <mo@coalla.ru>
2014-01-21 16:15:03 +0400

    .

---
+++ assets/javascripts/redirect.js
@@ -0,0 +1,4 @@
+var url = {
+  ios: 'https://itunes.apple.com/us/app/ping-app/id694666193?l=ru&ls=1&mt=8',
+  android: 'https://play.google.com/store'
+}
--- get/ios/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-  header('Location: https://itunes.apple.com/us/app/ping-app/id694666193?l=ru&ls=1&mt=8');
-?>
