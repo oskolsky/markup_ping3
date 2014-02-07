0a1a866ed51d610bdc01e7513ac0364f27d2449e
Maxim Oskolsky <mo@coalla.ru>
2014-02-07 15:35:42 +0400

    New lang file

--- assets/javascripts/lang.js
+++ assets/javascripts/lang.js
@@ -1,16 +1,16 @@
-﻿Lang = Backbone.Model.extend({
+Lang = Backbone.Model.extend({
 
   defaults: {
     RU:  {
-      confidential: "Конфиденциальный",
-      messenger:    "мессенджер",
+      confidential: "Мессенджер",
+      messenger:    "нового поколения",
       terms:        "Условия использования",
       feedback:     "Обратная связь",
       blog:         "Блог"
     },
 
     EN:  {
-      confidential: "Confidential",
+      confidential: "Next generation",
       messenger:    "messenger",
       terms:        "Terms of Use",
       feedback:     "Feedback",
@@ -18,55 +18,55 @@
     },
 
     FR:  {
-      confidential: "Messenger",
-      messenger:    "confidentiel",
+      confidential: "Messagerie de la",
+      messenger:    "nouvelle génération",
       terms:        "Conditions d’utilisation",
       feedback:     "Commentaires",
       blog:         "Blog"
     },
 
     ES:  {
-      confidential: "Mensajero",
-      messenger:    "confidencial",
+      confidential: "Mensajería de",
+      messenger:    "nueva generación",
       terms:        "Términos de uso",
       feedback:     "Comentarios",
       blog:         "Blog"
     },
 
     IT:  {
-      confidential: "Messaggistica",
-      messenger:    "riservata",
+      confidential: "Messenger di",
+      messenger:    "prossima generazione",
       terms:        "Condizioni per l'utilizzo",
       feedback:     "Feedback",
       blog:         "Blog"
     },
 
     PT:  {
-      confidential: "Mensageiro",
-      messenger:    "confidencial",
+      confidential: "Mensageiro de",
+      messenger:    "próxima geração",
       terms:        "Termos de utilização",
       feedback:     "Comentários",
       blog:         "Blogue"
     },
 
     DE:  {
-      confidential: "Vertraulicher",
-      messenger:    "Messenger",
+      confidential: "Messenger der",
+      messenger:    "nächsten generation",
       terms:        "Nutzungsbedingungen",
       feedback:     "Feedback",
       blog:         "Blog"
     },
 
     NL:  {
-      confidential: "Vertrouwelijke",
-      messenger:    "messenger",
+      confidential: "Volgende",
+      messenger:    "generatie messenger",
       terms:        "Gebruiksvoorwaarden",
       feedback:     "Feedback",
       blog:         "Blog"
     },
 
     KO:  {
-      confidential: "비공개",
+      confidential: "차세대",
       messenger:    "메신저",
       terms:        "서비스 계약",
       feedback:     "피드백",
@@ -75,48 +75,48 @@
    
 
     TH:  {
-      confidential: "เครื่อมือส่งข้อความที่เป็นความลับ",
-      messenger:    "&nbsp;",
+      confidential: "รุ่นต่อไป",
+      messenger:    "ผู้ส่งสาร",
       terms:        "ข้อกำหนดการใช้",
       feedback:     "คำติชม",
       blog:         "สมุดบันทึกบนเว็บ"
     },
 
     ID:  {
-      confidential: "Pembawa",
-      messenger:    "pesan rahasia",
+      confidential: "Generasi berikutnya",
+      messenger:    "utusan",
       terms:        "Ketentuan penggunaan",
       feedback:     "Tanggapan",
       blog:         "Blog"
     },
 
     VI:  {
-      confidential: "Tin",
-      messenger:    "bí mật",
+      confidential: "Thế hệ tiếp theo",
+      messenger:    "tin",
       terms:        "Điều khoản sử dụng",
       feedback:     "Phản hồi",
       blog:         "Blog"
     },
 
     AR:  {
-      confidential: "مرسل",
-      messenger:    "سري",
+      confidential: "برنامج الماسنجر",
+      messenger:    "من الجيل القادم",
       terms:        "شروط الاستخدام",
       feedback:     "ملاحظات",
       blog:         "مدوّنة"
     },
 
     ZH:  {
-      confidential: "私密通讯工具",
-      messenger:    "&nbsp;",
+      confidential: "下一代",
+      messenger:    "即时消息软件;",
       terms:        "利用規約",
       feedback:     "反馈",
       blog:         "博客"
     },
 
     JA:  {
-      confidential: "機密メッセンジャー",
-      messenger:    "&nbsp;",
+      confidential: "次世代の",
+      messenger:    "メッセンジャー",
       terms:        "利用規約",
       feedback:     "フィードバック",
       blog:         "ブログ"
