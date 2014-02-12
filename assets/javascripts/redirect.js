73386998534fea0858cd2fae9b9262b6cd1aee99
Maxim Oskolsky <mo@coalla.ru>
2014-02-12 17:41:29 +0400

    .

--- assets/javascripts/redirect.js
+++ assets/javascripts/redirect.js
@@ -1,4 +1,4 @@
 var url = {
   ios: 'https://itunes.apple.com/us/app/ping-app/id694666193?l=ru&ls=1&mt=8',
-  android: 'https://play.google.com/store'
+  android: 'https://play.google.com/store/apps/details?id=me.pingapp'
 }
